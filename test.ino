ed8fd1832a981b237fdcbd8b340d64413ad2470a
RohannNamay <105261723+RohannNamay@users.noreply.github.com>
2025-08-04 17:01:55 +1000

    Update test.ino

--- test.ino
+++ test.ino
@@ -10,9 +10,15 @@ void setup() {
 void loop() {
   // Turn LED on
   Write_GPIO('A', 17, HIGH);
+  bool pinState = Read_GPIO('A', 17); // Read state after setting HIGH
+  Serial.print("LED ON, Pin state: ");
+  Serial.println(pinState);
   delay(1000);
 
   // Turn LED off
   Write_GPIO('A', 17, LOW);
+  pinState = Read_GPIO('A', 17); // Read state after setting LOW
+  Serial.print("LED OFF, Pin state: ");
+  Serial.println(pinState);
   delay(1000);
 }
